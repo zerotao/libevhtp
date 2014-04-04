d0d10a11b205862a70ea3d7c5e779a658b6481e0
Mark Ellzey <socket@gmail.com>
2014-04-04 14:46:50 -0400

    Add evhtp.h for backwards compatability

---
+++ src/evhtp.h
@@ -0,0 +1,16 @@
+#ifndef __EVHTP_BASE_H__
+#define __EVHTP_BASE_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <evhtp2/evhtp-config.h>
+#include <evhtp2/evhtp.h>
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
