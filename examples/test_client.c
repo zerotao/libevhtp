f0763cf9814631c8ab0b5589adc54920a6b7d5c1
Mark Ellzey <socket@gmail.com>
2014-05-21 12:40:54 -0400

    test_client, make two requests on saame connection.

--- examples/test_client.c
+++ examples/test_client.c
@@ -6,8 +6,29 @@
 #include <inttypes.h>
 #include <evhtp.h>
 
+static evhtp_res print_data(evhtp_req_t * req, struct evbuffer * buf, void * arg);
+static evhtp_res print_new_chunk_len(evhtp_req_t * req, uint64_t len, void * arg);
+static evhtp_res print_chunks_complete(evhtp_req_t * req, void * arg);
+static evhtp_res print_chunk_complete(evhtp_req_t * req, void * arg);
+
 static void
 req_cb(evhtp_req_t * req, void * arg) {
+    struct event_base * evbase  = evhtp_req_get_evbase(req);
+    evhtp_conn_t      * conn    = evhtp_req_get_conn(req);
+    evhtp_req_t       * n_req   = evhtp_req_new(req_cb, evbase);
+    evhtp_hdrs_t      * headers = evhtp_req_get_headers_out(n_req);
+
+    evhtp_req_set_hook(n_req, evhtp_hook_on_read, print_data, evbase);
+    evhtp_req_set_hook(n_req, evhtp_hook_on_new_chunk, print_new_chunk_len, NULL);
+    evhtp_req_set_hook(n_req, evhtp_hook_on_chunk_complete, print_chunk_complete, NULL);
+    evhtp_req_set_hook(n_req, evhtp_hook_on_chunks_complete, print_chunks_complete, NULL);
+
+    evhtp_hdrs_add_header(headers, evhtp_hdr_new("Host", "ieatfood.net", 0, 0));
+    evhtp_hdrs_add_header(headers, evhtp_hdr_new("User-Agent", "libevhtp", 0, 0));
+    evhtp_hdrs_add_header(headers, evhtp_hdr_new("Connection", "close", 0, 0));
+
+    evhtp_make_req(conn, n_req, evhtp_method_GET, "/");
+
     printf("hi %zu\n", evbuffer_get_length(evhtp_req_buffer_in(req)));
 }
 
@@ -58,7 +79,7 @@ main(int argc, char ** argv) {
 
     evhtp_hdrs_add_header(headers, evhtp_hdr_new("Host", "ieatfood.net", 0, 0));
     evhtp_hdrs_add_header(headers, evhtp_hdr_new("User-Agent", "libevhtp", 0, 0));
-    evhtp_hdrs_add_header(headers, evhtp_hdr_new("Connection", "close", 0, 0));
+    evhtp_hdrs_add_header(headers, evhtp_hdr_new("Connection", "keep-alive", 0, 0));
 
     evhtp_make_req(conn, req, evhtp_method_GET, "/");
 
