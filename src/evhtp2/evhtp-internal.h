7ee47d02d760a91926d37c5963110e5e29bba327
Mark Ellzey <socket@gmail.com>
2014-11-15 05:55:52 -0500

    Backpatch from a2ebece503 (field padding)

--- src/evhtp2/evhtp-internal.h
+++ src/evhtp2/evhtp-internal.h
@@ -40,24 +40,24 @@ struct evhtp_defaults {
  * @brief main structure containing all configuration information
  */
 struct evhtp {
-    struct event_base     * evbase;               /**< the initialized event_base */
-    struct evconnlistener * server;               /**< the libevent listener struct */
-    evhtp_t               * parent;               /**< only when this is a vhost */
-    char                  * server_name;          /**< the name included in Host: responses */
-    void                  * arg;                  /**< user-defined evhtp_t specific arguments */
-    uint16_t                bev_flags;            /**< bufferevent flags to use on bufferevent_*_socket_new() */
+    struct event_base     * evbase;           /**< the initialized event_base */
+    struct evconnlistener * server;           /**< the libevent listener struct */
+    evhtp_t               * parent;           /**< only when this is a vhost */
+    char                  * server_name;      /**< the name included in Host: responses */
+    void                  * arg;              /**< user-defined evhtp_t specific arguments */
+    uint16_t                bev_flags;        /**< bufferevent flags to use on bufferevent_*_socket_new() */
     uint64_t                max_body_size;
     uint32_t                max_keepalive_reqs;
-    uint8_t                 disable_100_cont : 1; /**< if set, evhtp will not respond to Expect: 100-continue */
+    uint8_t                 disable_100_cont; /**< if set, evhtp will not respond to Expect: 100-continue */
 
 #ifdef EVHTP_ENABLE_SSL
-    evhtp_ssl_ctx_t * ssl_ctx;                    /**< if ssl enabled, this is the servers CTX */
+    evhtp_ssl_ctx_t * ssl_ctx;                /**< if ssl enabled, this is the servers CTX */
     evhtp_ssl_cfg_t * ssl_cfg;
 #endif
 
 #ifdef EVHTP_ENABLE_EVTHR
-    evhtp_thr_pool_t   * thr_pool;                /**< conn threadpool */
-    pthread_mutex_t    * lock;                    /**< parent lock for add/del cbs in threads */
+    evhtp_thr_pool_t   * thr_pool;            /**< conn threadpool */
+    pthread_mutex_t    * lock;                /**< parent lock for add/del cbs in threads */
     evhtp_thread_init_cb thread_init_cb;
     void               * thread_init_cbarg;
 #endif
@@ -80,21 +80,22 @@ struct evhtp_conn {
     struct bufferevent * bev;
     struct event       * resume_ev;
     struct sockaddr    * saddr;
-    struct timeval       recv_timeo;          /**< conn read timeouts (overrides global) */
-    struct timeval       send_timeo;          /**< conn write timeouts (overrides global) */
-    evhtp_req_t        * req;                 /**< the req currently being processed */
+    struct timeval       recv_timeo;        /**< conn read timeouts (overrides global) */
+    struct timeval       send_timeo;        /**< conn write timeouts (overrides global) */
+    evhtp_req_t        * req;               /**< the req currently being processed */
     evhtp_hooks_t      * hooks;
     evhtp_parser       * parser;
-    uint8_t              error         : 1;
-    uint8_t              owner         : 1;   /**< set to 1 if this structure owns the bufferevent */
-    uint8_t              vhost_via_sni : 1;   /**< set to 1 if the vhost was found via SSL SNI */
-    uint8_t              free_conn     : 1;
-    uint8_t              connected     : 1;
-    uint64_t             max_body_size;
-    uint64_t             body_bytes_read;
-    uint64_t             num_reqs;
-    evhtp_type           type;                /**< server or client */
-    evhtp_pause_state    paused;
+    uint8_t              error         : 1,
+                         owner         : 1, /**< set to 1 if this structure owns the bufferevent */
+                         vhost_via_sni : 1, /**< set to 1 if the vhost was found via SSL SNI */
+                         free_conn     : 1,
+                         connected     : 1,
+                         pad           : 3;
+    uint64_t          max_body_size;
+    uint64_t          body_bytes_read;
+    uint64_t          num_reqs;
+    evhtp_type        type;                 /**< server or client */
+    evhtp_pause_state paused;
 
 #ifdef EVHTP_ENABLE_EVTHR
     evhtp_thr_t * thread;
@@ -103,34 +104,36 @@ struct evhtp_conn {
     evhtp_ssl_t * ssl;
 #endif
 
-    TAILQ_HEAD(, evhtp_req) pending;       /**< client pending data */
+    TAILQ_HEAD(, evhtp_req) pending; /**< client pending data */
 };
 
 /**
  * @brief a structure containing all information for a http req.
  */
 struct evhtp_req {
-    evhtp_t         * htp;                 /**< the parent evhtp_t structure */
-    evhtp_conn_t    * conn;                /**< the associated conn */
-    evhtp_hooks_t   * hooks;               /**< req specific hooks */
-    evhtp_uri_t     * uri;                 /**< req URI information */
-    struct evbuffer * buffer_in;           /**< buffer containing data from client */
-    struct evbuffer * buffer_out;          /**< buffer containing data to client */
-    evhtp_hdrs_t    * headers_in;          /**< headers from client */
-    evhtp_hdrs_t    * headers_out;         /**< headers to client */
-    evhtp_proto       proto;               /**< HTTP protocol used */
-    evhtp_method      method;              /**< HTTP method used */
-    evhtp_res         status;              /**< The HTTP response code or other error conditions */
-    uint8_t           keepalive : 1;       /**< set to 1 if the conn is keep-alive */
-    uint8_t           finished  : 1;       /**< set to 1 if the req is fully processed */
-    uint8_t           chunked   : 1;       /**< set to 1 if the req is chunked */
-    uint8_t           error     : 1;
-    uint8_t           websock   : 1;
-    evhtp_ws_parser * ws_parser;
+    evhtp_t         * htp;           /**< the parent evhtp_t structure */
+    evhtp_conn_t    * conn;          /**< the associated conn */
+    evhtp_hooks_t   * hooks;         /**< req specific hooks */
+    evhtp_uri_t     * uri;           /**< req URI information */
+    struct evbuffer * buffer_in;     /**< buffer containing data from client */
+    struct evbuffer * buffer_out;    /**< buffer containing data to client */
+    evhtp_hdrs_t    * headers_in;    /**< headers from client */
+    evhtp_hdrs_t    * headers_out;   /**< headers to client */
+    evhtp_proto       proto;         /**< HTTP protocol used */
+    evhtp_method      method;        /**< HTTP method used */
+    evhtp_res         status;        /**< The HTTP response code or other error conditions */
+    uint8_t           keepalive : 1, /**< set to 1 if the conn is keep-alive */
+                      finished  : 1, /**< set to 1 if the req is fully processed */
+                      chunked   : 1, /**< set to 1 if the req is chunked */
+                      error     : 1,
+                      websock   : 1,
+                      pad       : 3;
 
     uint8_t           cb_has_websock;
-    evhtp_callback_cb cb;                  /**< the function to call when fully processed */
-    void            * cbarg;               /**< argument which is passed to the cb function */
+    evhtp_ws_parser * ws_parser;
+
+    evhtp_callback_cb cb;            /**< the function to call when fully processed */
+    void            * cbarg;         /**< argument which is passed to the cb function */
 
     TAILQ_ENTRY(evhtp_req) next;
 };
@@ -185,8 +188,8 @@ struct evhtp_kv {
     size_t klen;
     size_t vlen;
 
-    uint8_t k_heaped : 1; /**< set to 1 if the key can be free()'d */
-    uint8_t v_heaped : 1; /**< set to 1 if the val can be free()'d */
+    uint8_t k_heaped; /**< set to 1 if the key can be free()'d */
+    uint8_t v_heaped; /**< set to 1 if the val can be free()'d */
 
     TAILQ_ENTRY(evhtp_kv) next;
 };
